2ab32f90e7d077551632d68b6220601df1645efd
Jorge Vicens <gromit.jvv@gmail.com>
2020-01-23 15:41:56 +0100

    añadidos modulos y componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+// modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// componentes
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
