ced83bf1c787874b77b2a2dba0b4587dffea7d59
Jorge Vicens <gromit.jvv@gmail.com>
2020-02-04 12:11:36 +0100

    añadido Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // componentes
 import { AppComponent } from './app.component';
@@ -48,7 +49,8 @@ import { SearchComponent } from './website/components/search/search.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
