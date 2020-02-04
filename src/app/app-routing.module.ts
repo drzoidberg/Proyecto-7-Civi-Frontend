b7a56ccb2244e3a4ccc5d06c66f53c9d17b3d4e3
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-02-04 14:32:50 +0100

    Añadido rutas y funcionalidad al header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginEmpresaComponent } from './website/views/login-empresa/login-empresa.component';
+import { LoginUsuarioComponent } from './website/views/login-usuario/login-usuario.component';
+import { HomeComponent } from './website/views/home/home.component';
 
 
 const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home",  component:HomeComponent },
+  {path:"login-empresa", component:LoginEmpresaComponent},
+  {path:"login-candidato", component:LoginUsuarioComponent}
+  // {path:"usuarios/login", component:LoginComponent},
+  // {path:"usuario/registro", component:RegistroComponent}
 ];
 
 @NgModule({
--- src/app/website/views/login-usuario/login-usuario.component.ts
+++ src/app/website/views/login-usuario/login-usuario.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUsuarioComponent implements OnInit {
 
+  usuario:Login={
+    "email":'',
+    "contrasenia":''
+  }
+
   constructor() { }
 
   ngOnInit() {
