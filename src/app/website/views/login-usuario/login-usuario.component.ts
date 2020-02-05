3d680f8cca223c64e2fc90750b3a62fa86d9526c
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-02-05 10:39:44 +0100

    Añadido persistencia de usuario en el login

--- src/app/website/views/login-usuario/login-usuario.component.ts
+++ src/app/website/views/login-usuario/login-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Login } from 'src/app/models/login.model';
 // import { WebsiteService } from '../../shared/services/website.service';
 import { WebsiteService } from '../../services/website.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-usuario',
@@ -15,10 +16,19 @@ export class LoginUsuarioComponent implements OnInit {
     "contrasenia":''
   }
 
-  constructor(private website:WebsiteService) { }
+  constructor(private website:WebsiteService , private router:Router) { }
 
   crearUsuario(){
-    this.website.loginUsuario(this.usuario).subscribe(res=> console.log(res));
+    this.website.loginUsuario(this.usuario).subscribe(res=>   {
+      let mensaje= 'Contrasenia o correo invalido';
+      if((res !== mensaje) ){
+      localStorage.setItem('usuario', res), setTimeout(() => {
+    location.reload(true);
+  }, 1000),  setTimeout(() => {
+    this.router.navigate(['/'])
+  }, 999)}else{
+    alert('Los datos no coinciden')
+  }});
   }
   ngOnInit() {
   }
