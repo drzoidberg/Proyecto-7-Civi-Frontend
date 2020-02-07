6cd9b4f9efa7190922928d4ad256783788376254
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-02-07 12:07:47 +0100

    Añadido estetica de solicitud empresa y correcion de bug

--- src/app/cms/components/dashboard-empresa-solicitud/dashboard-empresa-solicitud.component.ts
+++ src/app/cms/components/dashboard-empresa-solicitud/dashboard-empresa-solicitud.component.ts
@@ -14,13 +14,12 @@ solicitudes:Array<Solicitudesempresa>=[];
   constructor(private cms:CmsService) { }
   borrarSolicitud(id){
     console.log('boton pulsado')
-    this.cms.borrarSolicitud(id, this.Empresa).subscribe(res=> console.log(res))
+    this.cms.borrarSolicitud(id, this.Empresa).subscribe(res=> {console.log(res), location.reload(true);})
   }
   cambiarSolicitud(respuesta, numeroSolicitud){
     console.log(respuesta, numeroSolicitud);
     this.cms.administrarSolicitud(respuesta, numeroSolicitud, this.Empresa).subscribe(res=>{
-      console.log(res), location.reload(true);
-    })
+      console.log(res); location.reload(true);})
   }
   ngOnInit() {
     this.cms.VerSolicitudesEmpresa(this.Empresa).subscribe(res => this.solicitudes.push(res));
